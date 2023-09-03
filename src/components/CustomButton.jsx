b42c5dfabf699103ffb8db35f45a61553eabe033
codewithmalik <codewithmalik.dev@gmail.com>
2023-09-03 22:23:25 +0100

    add link

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const CustomButton = () => {
   return (
     <div className="buttonContainer">
-      <Link to={'/contact'} className="title">
+      <Link to={'https://wa.link/l046yq'} className="title" target="_blank">
         Click here to proccess
       </Link>
     </div>
