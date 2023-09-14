2d757e1528db8bce390b6f6f56a1fc66a9cadd35
codewithmalik <codewithmalik.dev@gmail.com>
2023-09-14 16:21:04 +0100

    add facebook ads

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -18,7 +18,7 @@ const Location = () => {
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
           className="video"
         ></iframe>
       </div>
